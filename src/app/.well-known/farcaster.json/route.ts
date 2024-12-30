6cd672bb119ba8c11d8af1929bfb3e892407801a
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-12-30 18:46:32 +0200

    fix: dynamic account associations

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,15 +1,12 @@
 export async function GET() {
   const appUrl = process.env.APP_URL;
 
+  if (!appUrl) {
+    throw new Error("APP_URL is not set");
+  }
+
   const config = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjE2ODksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyNzM4QjIxY0I5NTIwMzM4RjlBMzc1YzNiOTcxQjE3NzhhZTEwMDRhIn0",
-      payload:
-        "eyJkb21haW4iOiI4YjM4LTEwMi0xMzUtMjQxLTIxNC5uZ3Jvay1mcmVlLmFwcCJ9",
-      signature:
-        "MHg3YmRjM2RjN2QwMDJkZjBiMmM3ZTc1YTQ5YTY5MjkzMGI0Y2U2M2ZhN2M0MWM1MTdmYzNmNzY5M2NjYmE4ZGQwMzMxNmZjYjI0Nzc5NzA4MzUxZjIzOGJmMmU3NWNiMjQzMmUwZDVjNmQxOWYwZGYzMWJmZjYwODQ2MjdhZjE2NjFi",
-    },
+    accountAssociation: accountAssociations[appUrl],
     frame: {
       version: "1",
       name: "dailysnap",
@@ -25,3 +22,20 @@ export async function GET() {
 
   return Response.json(config);
 }
+
+const accountAssociations = {
+  "https://irl.steer.fun": {
+    header:
+      "eyJmaWQiOjE2ODksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyNzM4QjIxY0I5NTIwMzM4RjlBMzc1YzNiOTcxQjE3NzhhZTEwMDRhIn0",
+    payload: "eyJkb21haW4iOiJpcmwuc3RlZXIuZnVuIn0",
+    signature:
+      "MHhmNzJkZTJiNjBlNDVkNTlmM2EzZDk1ZjQ4MjI5Yjg1MTdkZTFmNTk3OTkzMmVkMGJkOTU2YzAxZTIzYzkwZmEwNzJkY2E4MDk3NDhkYjM2NzRiNzhmMDk2MTAxMjkyZDBhYmE4ZDdjYTMzZTBmODQzZTljZDQyNWMwNTA1NjRiOTFj",
+  },
+  "https://8b38-102-135-241-214.ngrok-free.app": {
+    header:
+      "eyJmaWQiOjE2ODksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyNzM4QjIxY0I5NTIwMzM4RjlBMzc1YzNiOTcxQjE3NzhhZTEwMDRhIn0",
+    payload: "eyJkb21haW4iOiI4YjM4LTEwMi0xMzUtMjQxLTIxNC5uZ3Jvay1mcmVlLmFwcCJ9",
+    signature:
+      "MHg3YmRjM2RjN2QwMDJkZjBiMmM3ZTc1YTQ5YTY5MjkzMGI0Y2U2M2ZhN2M0MWM1MTdmYzNmNzY5M2NjYmE4ZGQwMzMxNmZjYjI0Nzc5NzA4MzUxZjIzOGJmMmU3NWNiMjQzMmUwZDVjNmQxOWYwZGYzMWJmZjYwODQ2MjdhZjE2NjFi",
+  },
+};
